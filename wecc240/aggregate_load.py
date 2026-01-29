3c79c10ffc6c049cdc8228b124a99b982f295f01
David P. Chassin <david.chassin@me.com>
2026-01-29 15:27:32 -0800

    Update docs

--- wecc240/aggregate_load.py
+++ wecc240/aggregate_load.py
@@ -1,63 +1,83 @@
-"""Aggregate county loads to network nodes
+"""Aggregate county/country loads to network nodes
 
-Syntax
-------
+Usage
+-----
     
     python3 aggregate_load.py [--debug] [--refresh] YEAR ...
 
+Example
+-------
 
+To get the aggregate load data for 2020 run the command
+
+    python2 aggregate_load.py 2020
+
+which outputs the following
+
+    /Users/david/GitHub/eudoxys/wecc240/wecc240/aggregate_load.py:62: PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
+      result[node] = data.loc[result.index,"load_MW"]
+    /Users/david/GitHub/eudoxys/wecc240/wecc240/aggregate_load.py:62: PerformanceWarning: DataFrame is highly fragmented.  This is usually the result of calling `frame.insert` many times, which has poor performance.  Consider joining all columns at once using pd.concat(axis=1) instead. To get a de-fragmented frame, use `newframe = frame.copy()`
+      result[node] = data.loc[result.index,"load_MW"]
+    WARNING:/Users/david/GitHub/eudoxys/wecc240/wecc240/aggregate_load.py:Mexico/9mtzm4.csv does not exist
+    WECC load for 2020
+    ------------------
+    Peak demand......... 164.4 GW
+    Peak day............ 2020-08-18 21:00:00 UTC
+    Total consumption... 928.7 TWh
 """
-import os
-import datetime as dt
-import sys
-import pytz
-import logging
 
-import pandas as pd
+if __name__ == "__main__":
+    import os
+    import datetime as dt
+    import sys
+    import pytz
+    import logging
 
-from loads.aggregate import aggregate
+    import pandas as pd
 
-pd.options.display.max_columns = None
-pd.options.display.width = None
-# pd.options.display.max_rows = None
+    from loads.aggregate import aggregate
 
-refresh = "--refresh" in sys.argv
-debug = "--debug" in sys.argv
+    pd.options.display.max_columns = None
+    pd.options.display.width = None
+    # pd.options.display.max_rows = None
 
-wecc240_gis = pd.read_csv("gis/wecc240.csv")
-canada = set(wecc240_gis[wecc240_gis.LAT>49].GEOHASH.values)
-mexico = set(wecc240_gis.set_index("GEOHASH").loc[["9mtzm4"]].index.values)
-omitted = canada|mexico
+    refresh = "--refresh" in sys.argv
+    debug = "--debug" in sys.argv
 
-locations,latlon = list(wecc240_gis.GEOHASH),list(zip(wecc240_gis.LAT,wecc240_gis.LON))
-targets = {x:latlon[locations.index(x)] for x in set(locations) if x not in omitted}
+    wecc240_gis = pd.read_csv("gis/wecc240.csv")
+    canada = set(wecc240_gis[wecc240_gis.LAT>49].GEOHASH.values)
+    mexico = set(wecc240_gis.set_index("GEOHASH").loc[["9mtzm4"]].index.values)
+    omitted = canada|mexico
 
-try:
-    year = int([x for x in sys.argv[1:] if not x.startswith("-")][1])
-except:
-    print("Syntax: python3 aggregate.py [--debug] [--refresh] YEAR",file=sys.stderr)
-    year = 2020
-    print(f"Using default aggregates for {year=} with {debug=}",flush=True)
+    locations,latlon = list(wecc240_gis.GEOHASH),list(zip(wecc240_gis.LAT,wecc240_gis.LON))
+    targets = {x:latlon[locations.index(x)] for x in set(locations) if x not in omitted}
 
-logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
-_logger = logging.getLogger(__file__)
+    try:
+        year = int([x for x in sys.argv[1:] if not x.startswith("-")][1])
+    except:
+        print("Syntax: python3 aggregate.py [--debug] [--refresh] YEAR",file=sys.stderr)
+        year = 2020
+        print(f"Using default aggregates for {year=} with {debug=}",flush=True)
 
-result = aggregate(targets,year,"elec_net_MW",refresh=refresh)[0]
+    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
+    _logger = logging.getLogger(__file__)
 
-for country,node in [("Canada",x) for x in canada] + [("Mexico",x) for x in mexico]:
-    file = f"{country}/{node}.csv"
-    if os.path.exists(file):
-        data = pd.read_csv(file,index_col=["timestamp"],parse_dates=["timestamp"])
-        result[node] = data.loc[result.index,"load_MW"]
-    else:
-        _logger.warning(f"{file} does not exist")
+    result = aggregate(targets,year,"elec_net_MW",refresh=refresh)[0]
 
-total = result[24:].sum(axis=1)
-print(f"WECC load for {year}")
-print("------------------")
-print(f"Peak demand......... {total.max()/1000:.1f} GW")
-peak_dt = total[total==total.max()].index.to_pydatetime()[0]
-print(f"Peak day............ {peak_dt.strftime('%Y-%m-%d %H:%M:%S %Z')}")
-print(f"Total consumption... {total.sum()/1e6:.1f} TWh")
+    for country,node in [("Canada",x) for x in canada] + [("Mexico",x) for x in mexico]:
+        file = f"{country}/{node}.csv"
+        if os.path.exists(file):
+            data = pd.read_csv(file,index_col=["timestamp"],parse_dates=["timestamp"])
+            result[node] = data.loc[result.index,"load_MW"]
+        else:
+            _logger.warning(f"{file} does not exist")
 
-result[sorted(result.columns)].to_csv("wecc240_2020_load.csv",index=True,header=True)
+    total = result[24:].sum(axis=1)
+    print(f"WECC load for {year}")
+    print("------------------")
+    print(f"Peak demand......... {total.max()/1000:.1f} GW")
+    peak_dt = total[total==total.max()].index.to_pydatetime()[0]
+    print(f"Peak day............ {peak_dt.strftime('%Y-%m-%d %H:%M:%S %Z')}")
+    print(f"Total consumption... {total.sum()/1e6:.1f} TWh")
+
+    result[sorted(result.columns)].to_csv("wecc240_2020_load.csv",index=True,header=True)
