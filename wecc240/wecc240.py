76182a77e62ec295e48ce3a10130f7ceebc965c2
David P. Chassin <david.chassin@me.com>
2026-06-24 07:31:09 -0700

    Add 2020 model with loads and timeseries simulation

--- wecc240/wecc240.py
+++ wecc240/wecc240.py
@@ -8,9 +8,12 @@ import numpy as np
 import pandas as pd
 
 import pypower_sim as ps
+from pypower_sim.ppmodel import idx_bus as bus
 from utilities import CommandLine
 
 E_OK = 0 # no error return code
+
+refresh = False # force refresh of intermediate files
 
 def main(argv=sys.argv):
     """Main entry function
@@ -34,57 +37,80 @@ def main(argv=sys.argv):
     model = ps.PPModel(cmdline.args[0],case=cmdline.args[0]+".py")
 
     # calculate reactive power load from real power load  
-    if not os.path.exists("data/wecc240_loadQ.csv.gz"):
-        def QfromP(P,pf_onpeak,pf_offpeak=1.0):
-            """Estimate reactive power based on real power and a linear power factor function"""
-            Pnorm = P/np.max(P)
-            pf = Pnorm*(pf_onpeak-pf_offpeak)+pf_offpeak
-            S = Pnorm / pf
-            Q = np.sqrt(S**2 - Pnorm**2)
-            return Q
-        P = pd.read_csv("data/wecc240_load.csv.gz",index_col=[0],parse_dates=[0])
+    if not os.path.exists("data/wecc240_bus_QD.csv.gz") or refresh:
+        print("Generating reactive power load",end="...",flush=True)
+        P = pd.read_csv("data/wecc240_bus_PD.csv.gz",index_col=[0],parse_dates=[0])
+        busmap = {str(int(y)):x for x,y in enumerate(model.case["bus"][:,bus.BUS_I])}
         Q = []
+        qfactor = model.case["bus"][:,bus.QD] / model.case["bus"][:,bus.PD]
         for column in P.columns:
             p = P[column].ffill().bfill()
-            q = QfromP(p,0.97)
+            q = p * qfactor[busmap[column]]
             Q.append(pd.DataFrame(
                 data={
                 column: q,
                 },
-                index=P.index))
-        pd.concat(Q,axis=1).to_csv("data/wecc240_loadQ.csv.gz",index=True,compression="gzip")
+                index=P.index).ffill().bfill())
+        Q = pd.concat(Q,axis=1)
+        Q.to_csv("data/wecc240_loadQ.csv.gz",index=True,compression="gzip")
+        print("ok",flush=True)
 
     datamgr = ps.PPData(model)
     datamgr.set_input(
         name="bus",
         column="PD",
-        file="data/wecc240_load.csv.gz",
+        file="data/wecc240_bus_PD.csv.gz",
         mapping="BUS_I",
+        scale=1/model.case["baseMVA"]
         )
     datamgr.set_input(
         name="bus",
         column="QD",
-        file="data/wecc240_loadQ.csv.gz",
+        file="data/wecc240_bus_QD.csv.gz",
         mapping="BUS_I",
+        scale=1/model.case["baseMVA"]
+        )
+    datamgr.set_output(
+        name="branch",
+        column="PF",
+        file="data/wecc240_branch_PF.csv",
+        scale=model.case["baseMVA"]
+        )
+    datamgr.set_output(
+        name="branch",
+        column="QF",
+        file="data/wecc240_branch_QF.csv",
+        scale=model.case["baseMVA"]
+        )
+    datamgr.set_output(
+        name="branch",
+        column="PT",
+        file="data/wecc240_branch_PT.csv",
+        scale=model.case["baseMVA"]
+        )
+    datamgr.set_output(
+        name="branch",
+        column="QT",
+        file="data/wecc240_branch_QT.csv",
+        scale=model.case["baseMVA"]
         )
 
     solver = ps.PPSolver(model)
     oce = ps.OceOptions()
     errors = solver.run_timeseries(
         start="2020-08-01 07:00:00+0000",
-        end="2020-08-31 06:00:00+0000",
+        end="2020-08-08 06:00:00+0000",
         freq="1h",
         progress=lambda *x,**y:print(f"Processing {y['event']} {y['timestamp']} ({len(y['errors'])} errors so far)",flush=True),
         use_acopf=None,
-        with_oce=oce,
-        stop_on_fail=False,
+        stop_on_fail=True,
+        violations=True,
         )
-    print(f"Done with {len(errors)} errors")
-
+    print(f"Done with {'no' if errors is None else len(errors)} errors")
     
     return E_OK
 
 
 if __name__ == '__main__':
     
-    sys.exit(main(["wecc240_2011"]))
+    sys.exit(main(["wecc240_2018"]))
