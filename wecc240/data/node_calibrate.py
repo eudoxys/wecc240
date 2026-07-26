0925d7752c42a697a3284c79539cf67a7428ec07
David P. Chassin <dchassin@eudoxys.com>
2026-07-26 08:02:39 -0700

    Update node_calibrate.py

--- wecc240/data/node_calibrate.py
+++ wecc240/data/node_calibrate.py
@@ -8,7 +8,24 @@ def cvx_options(**kwargs):
     
     Arguments
     ---------
-    - `**kwargs`: modifications to default to appy to this option set"""
+    - `**kwargs`: modifications to default to appy to this option set
+
+    Examples
+    --------
+
+    To get the default `cvxpy.Problem.solve` options use the following.
+
+        cvx_options()
+
+    To get the default options with the "highs" solver use the following.
+
+        cvx_options(solver="highs")
+
+    To get the default optios with an interation limit of 50000 use the
+    following.
+
+        cvx_options(max_iter=50000)
+    """
     options = {
         "solver": "clarabel",
         "verbose": True,
@@ -17,27 +34,56 @@ def cvx_options(**kwargs):
     return options
 
 def _solver(X:np.array,
-               E_target:float=None,
-               P_target:float=None,
-               *,
-               gamma:float=1.0,
-               mu:float=0.0,
-               lam:float=0.0,
-               eps:float=1e-3,
-               options:dict=cvx_options(),
-              ):
-    """Solve the single energy/power target using scale only
+            E_target:float|list[tuple[list[int],float]]=None,
+            P_target:float|list[tuple[list[int],float]]=None,
+            *,
+            gamma:float=1.0,
+            mu:float=0.0,
+            lam:float=0.0,
+            eps:float=1e-3,
+            options:dict=cvx_options(),
+            ):
+    """Solve the sum/max target problem using scale and offset
 
     Arguments
     ---------
     - `X(T, n)`: nonnegative array of time-series signals
-    - `E_target`: scalar energy target
-    - `P_target`: scalar power target
+    - `E_target`: scalar sum of rows target(s)
+    - `P_target`: scalar max of sum columns target(s)
     - `gamma`: shape fidelity (anti-crushing) -- primary regularizer
-    - `mu`: energy target objective hyperparameter (recovers energy equality)   
-    - `lam`: power target objective hyperparameter (presses peak to ceiling)
-    - `eps`: small symmetric redice guaranteed a unique solution when zero or constant columns are present
+    - `mu`: `E_target` objective hyperparameter (recovers `E_target` equality)   
+    - `lam`: `P_target` objective hyperparameter (presses `P_target` to max)
+    - `eps`: small symmetric reduce guaranteed a unique solution when zero or
+      constant columns are present
+
+    Description
+    -----------
+
+    This solver scales and offsets the array X such that the total of the rows
+    matches `E_target` and the maximum across the rows of the sums of the rows
+    matches `P_target`. Since exact matching of both is typically infeasible, 
+    the solver uses the hyper-parameters `mu` and `lam` to prioritize these
+    against the fidelity to the overall shape of `X`, which is controlled by the
+    primary regularizer `gamma`. The hyper-parameters `mu` and `lam` work as
+    follows.
+
+    - `mu`: controls how strongly the solver recovers `E_target`.
+
+    - `lam`: controls how strongly the solver recover `P_target`.
+
+    The hyper-parameter `eps` is used to guarantee
+    a unique solution when a zero or constant column is present in `X`. 
+
+    When multiple `E_target` or `P_target` objectives are sought, these can be
+    specified as a list of columns with the target value, e.g.,
+
+        [([1,2,3], 1.23),
+         ([4,5,6], 4.56),
+         ]
+
+    Note that a column may appear in more than one target if desired.
     """
+    
     T, n = X.shape
 
     a = cp.Constant(X.sum(axis=0),name='a')     # column energy weights, shape (n,)
@@ -64,15 +110,15 @@ def _solver(X:np.array,
     eps = cp.Constant(eps,name='eps')
 
     objective = cp.Minimize(
-        gamma * cp.sum_squares(Y - X)           # == ||X diag(s) + 1 b^T - X||_F^2
-        + mu * cp.square(energy - E)
-        - lam * cp.sum(b)
+        gamma * cp.sum_squares(Y - X) / T**2           # == ||X diag(s) + 1 b^T - X||_F^2
+        + mu * cp.square(energy - E) / T**2
+        - lam * cp.sum(b) / n
         + eps * (cp.sum_squares(s - 1) + cp.sum_squares(b))
     )
     constraints = [
         peak <= P,                              # power inequality (convex relaxation)
-        cp.multiply(s, c) + b >= 0,       # output nonnegativity (reduced form)
-        # equivalent full form: Y >= 0
+        cp.multiply(s, c) + b >= 0,             # output nonnegativity (reduced form)
+                                                # equivalent full form: Y >= 0
     ]
 
     prob = cp.Problem(objective, constraints)
